ca7095da6badece145bfcffea529d98ffc64d7a5
MOHSIN-JAWADWALA <mohsinjawadwala8@gmail.com>
2026-01-10 13:07:18 +0530

    Initail commit

---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import { ENV } from './lib/env.js'
+import path from 'path'
+
+const app = express()
+
+const __dirname = path.resolve()
+
+app.get("/books", (req,res)=> {
+    res.status(200).json({msg : "api is books"})
+})
+
+
+
+if(ENV.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("/{*any}" , (req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend/","dist","index.html"))
+    })
+}
+
+app.listen(ENV.PORT, () => {
+    console.log("server up on " ,ENV.PORT)
+})
