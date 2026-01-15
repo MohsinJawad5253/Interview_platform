c74cc9eb88f690acaba650e81720718b7127c960
MOHSIN-JAWADWALA <mohsinjawadwala8@gmail.com>
2026-01-15 13:54:52 +0530

    add landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,20 @@ import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser }
 import { Navigate, Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage'
 import ProblemsPage from './pages/ProblemsPage'
+import DashBoard from './pages/Dashboard'
 import { Toaster } from 'react-hot-toast'
 
 function App() {
 
-  const { isSignedIn } = useUser()
+  const { isSignedIn , isLoaded} = useUser()
+
+  if(!isLoaded) return null
 
   return (
     <>
       <Routes className='bg-black h-screen p-10'>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"}/>} />
+        <Route path="/dashboard" element={isSignedIn? <DashBoard /> : <Navigate to={"/"}/>} />
         <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
       </Routes>
       <Toaster />
